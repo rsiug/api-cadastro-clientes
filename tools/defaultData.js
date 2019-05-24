91e5cd61e79898f22d5e35175e86382411ac1d1a
Stephan <stephan.jordao@gmail.com>
2019-05-23 23:30:11 -0300

    Adding json db.

---
+++ tools/defaultData.js
@@ -0,0 +1,25 @@
+const clientes = [
+  {
+    id: 1,
+    nome: "João",
+    dataNascimento: "1988-01-01",
+    filhos: 2,
+    endereco: "Rua dos Ciprestes, 123"
+  },
+  {
+    id: 2,
+    nome: "Pedro",
+    dataNascimento: "1989-01-01",
+    filhos: 1,
+    endereco: "Rua das Samabaias, 321"
+  },
+  {
+    id: 3,
+    nome: "Kerleston",
+    dataNascimento: "1985-01-01",
+    filhos: 3,
+    endereco: "Rua do Viradouro, 999"
+  }
+];
+
+module.exports = { clientes };
